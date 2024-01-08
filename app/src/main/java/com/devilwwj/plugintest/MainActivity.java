4ae62e7355ee0e26ef2503e8abaacc9b6d72e34a
raymond-shiply <raymondhu@tencent.com>
2024-01-08 11:08:40 +0800

    修改为Shiply灰度升级Demo
    删除原有bugly betauploader gradle插件逻辑，改为shiply upgrde demo;

--- app/src/main/java/com/devilwwj/plugintest/MainActivity.java
+++ app/src/main/java/com/devilwwj/plugintest/MainActivity.java
@@ -1,13 +1,31 @@
 package com.devilwwj.plugintest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import com.tencent.upgrade.core.UpgradeManager;
+import com.tencent.upgrade.core.UpgradeReqCallbackForUserManualCheck;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private OnClickListener onClickListener = v -> {
+        switch (v.getId()) {
+            case R.id.btn_detect_upgrade:
+                manualDetectUpgrade();
+                break;
+            default:
+                break;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_detect_upgrade).setOnClickListener(onClickListener);
+    }
+
+    private void manualDetectUpgrade() {
+        UpgradeManager.getInstance().checkUpgrade(true, null, new UpgradeReqCallbackForUserManualCheck());
     }
 }
---
+++ app/src/main/java/com/devilwwj/plugintest/MyApplication.java
@@ -0,0 +1,41 @@
+package com.devilwwj.plugintest;
+
+import android.app.Application;
+import android.os.Build;
+import com.tencent.upgrade.bean.UpgradeConfig;
+import com.tencent.upgrade.core.DefaultUpgradeStrategyRequestCallback;
+import com.tencent.upgrade.core.UpgradeManager;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by raymondhu on 2024/1/5
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initShiplyUpgradeSDK();
+        autoDetectUpgrade();
+    }
+
+    private void initShiplyUpgradeSDK() {
+        Map<String, String> map = new HashMap<>();
+        map.put("UserAge", "18");
+        UpgradeConfig.Builder builder = new UpgradeConfig.Builder();
+        builder.appId("379d66a2c6") // 测试项目 appid
+                .appKey("44a2e6fe-7c5a-452a-9b55-49d3705b1f20") // 测试项目 appkey
+                .systemVersion(String.valueOf(Build.VERSION.SDK_INT)) // 用户手机系统版本，用于匹配shiply前端创建任务时设置的系统版本下发条件
+                .customParams(map) // 自定义属性键值对，用于匹配shiply前端创建任务时设置的自定义下发条件
+                .cacheExpireTime(1000 * 60 * 60 * 6) // 灰度策略的缓存时长（ms），如果不设置，默认缓存时长为1天
+                .internalInitMMKVForRDelivery(true) // 是否由sdk内部初始化mmkv(调用MMKV.initialize()),业务方如果已经初始化过mmkv，可以设置为false
+                .userId("xxx") // 用户Id,用于匹配shiply前端创建的任务中的体验名单以及下发条件中的用户号码包
+                .customLogger(new MyLogger()); // 日志实现接口，建议对接到业务方的日志接口，方便排查问题
+        UpgradeManager.getInstance().init(MyApplication.this, builder.build());
+    }
+
+    private void autoDetectUpgrade() {
+        UpgradeManager.getInstance().checkUpgrade(false, null, new DefaultUpgradeStrategyRequestCallback());
+    }
+}
